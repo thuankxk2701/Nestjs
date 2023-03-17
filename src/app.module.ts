9f952f854338f9b35c7f714b945fe7a73c7fe546
Nguyen Van Thuan <thuan.nv@evotek.vn>
2023-03-17 08:54:25 +0700

    Thuannv update hot HRM for nestjs

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,15 @@
-import { Module } from '@nestjs/common';
+import {MiddlewareConsumer, Module, NestModule} from '@nestjs/common';
 import { CustomersModule } from "./customers/customers.module";
 import { UsersModule } from "./users/users.module";
+import {LoggerMiddlewares} from "./Middlewares/Logger.middlewares";
 
 @Module({
   imports: [CustomersModule,UsersModule],
   controllers: [],
   providers: [],
 })
-export class AppModule {}
+export class AppModule implements NestModule{
+  configure(consumer: MiddlewareConsumer): any {
+    consumer.apply(LoggerMiddlewares).forRoutes("*")
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import {NEST_APP_PORT} from "./constants";
+declare const module: any;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api')
-  await app.listen(5001);
+  await app.listen(NEST_APP_PORT);
+  if(module.hot){
+    module.hot.accept();
+    module.hot.dispose(()=>app.close())
+  }
 }
 bootstrap();
